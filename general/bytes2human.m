b70b96ae92c317205dc4184514c6b3cd5ddd834b
Frank Fishburn <frankthefishburn@gmail.com>
2018-08-02 17:55:21 -0400

    Fix bytes2human when no output arguments

--- general/bytes2human.m
+++ general/bytes2human.m
@@ -8,6 +8,7 @@ scale = floor(log(bytes)/log(1024));
 if scale>5, scale=5; end
 str = [num2str(bytes/1024^scale) ' ' levels{scale+1}];
 if nargout==0
+    fprintf('%s\n',str);
     clear str;
 end
 end
