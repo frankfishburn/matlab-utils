4d25a764a15e9a19f2f63a9d52afcf5861927ae7
Frank Fishburn <frankthefishburn@gmail.com>
2018-11-19 15:52:08 -0500

    Add option for robust zscore computation in nanzscore

--- stats/nanzscore.m
+++ stats/nanzscore.m
@@ -1,10 +1,19 @@
-function z = nanzscore( data , flag , dim )
+function z = nanzscore( data , flag , dim , robust )
 % Same as builtin zscore() function but ignores NaN values
 %
-% z = nanzscore( data, [flag], [dim] );
+% z = nanzscore( data, [flag], [dim], [robust] );
 if nargin<2 || isempty(flag), flag = 0; end
 if nargin<3 || isempty(dim), dim = 1; end
+if nargin<4 || isempty(robust), robust = 0; end
 
-z = bsxfun( @rdivide , bsxfun( @minus , data , nanmean(data,dim) ) , nanstd(data,flag,dim) );
+if robust
+    mu = nanmedian(data,dim);
+    sigma = 1.4826 * mad(data,1,dim);
+else
+    mu = nanmean(data,dim);
+    sigma = nanstd(data,flag,dim);
+end
+
+z = (data-mu)./sigma;
 
 end
