a8c96eb2521d977dafee78d8066d2e80c11f5d6e
Frank Fishburn <frankthefishburn@gmail.com>
2018-11-19 17:46:55 -0500

    Allow generating polarline into subplots

--- plotting/polarline.m
+++ plotting/polarline.m
@@ -1,10 +1,11 @@
-function polarline(vals,labels)
+function polarline(vals,labels,ax)
 % An easy wrapper for categorical polar plots
 % Usage: polarline(values, labels)
+if nargin<3
+    f=figure;
+    ax=polaraxes(f);
+end
 x = linspace(0,2*pi,length(vals)+1);
-f=figure;
-ax=polaraxes(f);
 polarplot(ax,x,[vals(:)' vals(1)]);
-set(ax,'ThetaAxisUnits','rad','ThetaTick',x,'ThetaTickLabel',labels);
-
+set(ax,'ThetaAxisUnits','rad','ThetaTick',x,'ThetaTickLabel',labels,'RTickLabel',{});
 end
